dd6dd3b6fcd9a63f092df1f6b84d3ac58a8d37a8
akeda <akeda2@gmail.com>
2021-11-11 18:53:55 +0100

    initial commit

---
+++ gcount.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#LINE=30
+#SONGL="$1"
+#
+#a=$SONGL
+#s=0
+#m=0
+#while (( a > 0 )); do
+#	printf "#"; sleep 1
+#	let s+=1
+#	let m+=1
+#	if (( $s == 30 )); then
+#		printf "\n"
+#		s=0
+#	fi
+#	if (( $m == 60 )); then
+#		printf "\n\n"
+#		m=0
+#	fi
+#	let a-=1
+#done
+echo "1"
